e3b9963ee4fffa483e0293a3cdbb6c7eb712f632
InnovativeSoura <patrasouradipta3028@gmail.com>
2025-03-07 12:01:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const heart = document.getElementById('heart');
 const card = document.getElementById('card');
 const music = document.getElementById('music');
 
-let isOpen = false;
+let isOpen = true;
 
 // Function to handle opening and closing the card
 function toggleCard() {
